b1fe5810407f60d269a729637c6313b9cf4c79fc
rodri-ubuntu <rcastro@uma.es>
2024-05-16 10:17:43 +0200

    Prueba: aplicar filtro a la aceleración

--- src/teleop_franka_joy.cpp
+++ src/teleop_franka_joy.cpp
@@ -192,11 +192,19 @@ namespace teleop_franka_joy
                                           last_O_dP_EE_c,
                                           last_O_ddP_EE_c);
 
-    // Aplica el filtro de primer orden
+    // Aplica el filtro de primer orden a la velocidad
     std::array<double, 6> O_dP_EE_c_filtered = firstOrderFilter(O_dP_EE_c_limited, last_O_dP_EE_c, alpha_first_order);
 
+
+    // Aplicar filtro de primer orden a la aceleración
+    std::array<double, 6> O_ddP_EE_c_filtered = firstOrderFilter(calculateAceleration(O_dP_EE_c_filtered, last_O_dP_EE_c, Delta_t), last_O_ddP_EE_c, alpha_first_order);
+
+
     // Calcula aceleracion: (O_dP_EE_c[i]-last_O_dP_EE_c[i])/Delta_t
-    last_O_ddP_EE_c = calculateAceleration(O_dP_EE_c_filtered, last_O_dP_EE_c, Delta_t);
+    last_O_ddP_EE_c = O_ddP_EE_c_filtered;
+
+    // Aplicar el filtro de primer orden en la aceleracion
+
 
     // Almacena la velocidad como velocidad previa para el proximo ciclo
     last_O_dP_EE_c = O_dP_EE_c_filtered;
